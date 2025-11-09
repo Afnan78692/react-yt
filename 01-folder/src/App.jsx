df4fdd1ac7e979f809881ba301393d66f80fa445
MD AFNAN QUAMAR <mdafnankhan786@gmail.com>
2025-11-09 15:55:31 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>hello</div>
+    <div>hit here</div>
+    </>
+  )
+
+}
+
+export default App
---
+++ 1/app.js
@@ -0,0 +1,5 @@
+const a=10;
+const user= "khahen";
+const arr= [21,22,22];
+const skill= ["afnan", "khan", "zeyan"];
+export default skill; 
